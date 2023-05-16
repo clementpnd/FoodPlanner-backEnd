b66faed9d8b777f354f30a029318df9ecd7200c8
Clement <clement.pinard.2013@gmail.com>
2023-05-16 17:23:31 +0200

    route recette

--- models/recettes.js
+++ models/recettes.js
@@ -1,24 +1,22 @@
 const mongoose = require("mongoose");
 
-const ingredientsSchema = mongoose.model({
-    nom : String,
-    type : String,
-    quantite : String,
-    image : String,
-})
+const ingredientsSchema = mongoose.Schema({
+  nom: String,
+  type: String,
+  quantite: String,
+  image: String,
+});
 
 const recetteSchema = mongoose.Schema({
+  nom: String,
+  description: String,
+  temps: String,
+  difficulte: String,
+  instruction: [String],
+  image: String,
+  ingredients: ingredientsSchema,
+});
 
-    nom : String, 
-    description : String,
-    temps : String,
-    difficulte : String,
-    instruction : [String],
-    image : String,
-    ingredients : ingredientsSchema,
-})
-
-
-const Recette = mongoose.model('recettes', recetteSchema);
+const Recette = mongoose.model("recettes", recetteSchema);
 
 module.exports = Recette;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 const { checkBody } = require("../modules/checkBody");
-const uid2 = require('uid2');
-const bcrypt = require('bcrypt');
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 const User = require("../models/users");
 
@@ -44,125 +44,119 @@ router.post("/signup", (req, res) => {
   });
 });
 
-//Vérifie si un User est déja présent 
-router.post("/verify", (req, res)=>{
+//Vérifie si un User est déja présent
+router.post("/verify", (req, res) => {
   if (!checkBody(req.body, ["mail"])) {
     res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
   User.findOne({ mail: req.body.mail }).then((data) => {
     if (data !== null) {
-      res.json({ result: false, error: "User already exists" })
+      res.json({ result: false, error: "User already exists" });
+    } else {
+      res.json({ result: true });
     }
-    else{
-      res.json({result: true})
-    }
-
-})
+  });
 });
-
-router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['mail', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
+// route de connexion
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["mail", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
-  User.findOne({ mail: req.body.mail }).then(data => {
+  User.findOne({ mail: req.body.mail }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
       res.json({ result: true, token: data.token });
     } else {
-      res.json({ result: false, error: 'User not found or wrong password' });
+      res.json({ result: false, error: "User not found or wrong password" });
     }
-  })
-})
+  });
+});
 
-
-    // route GET qui récupère le nombre de personnes enregistré dans le profil pour réutiliser dans la semaine
-    router.get('/nbPersonne/:token', (req, res) => {
-      User.findOne({ token: req.params.token }).then(user => {
-        if (user === null) {
-          res.json({ result: false, error: 'User not found' });
-          return;
-        }
-        else { res.json({ result: true, nbPersonne: user.nbPersonne });}
-      });
-    });
-
-
- // route PUT qui permet de modifier la semaine type déjà enrgistrée
- router.put('/semaines/:token', (req, res) => {
-  User.findOne({ token: req.params.token }).then(user => {
+// route GET qui récupère le nombre de personnes enregistré dans le profil pour réutiliser dans la semaine
+router.get("/nbPersonne/:token", (req, res) => {
+  User.findOne({ token: req.params.token }).then((user) => {
     if (user === null) {
-      res.json({ result: false, error: 'User not found' });
+      res.json({ result: false, error: "User not found" });
+      return;
+    } else {
+      res.json({ result: true, nbPersonne: user.nbPersonne });
+    }
+  });
+});
+
+// route PUT qui permet de modifier la semaine type déjà enrgistrée
+router.put("/semaines/:token", (req, res) => {
+  User.findOne({ token: req.params.token }).then((user) => {
+    if (user === null) {
+      res.json({ result: false, error: "User not found" });
+      return;
+    } else {
+      User.updateOne(
+        { token: req.body.token },
+        {
+          $set: {
+            semaines: {
+              jour: req.body.jour,
+              nbPersonneSemaine: req.body.nbPersonneSemaine,
+            },
+          },
+        }
+      )
+      .then((data) => {
+        console.log(data);
+        res.json({ result: true, semaines: data });
+      });
+    }
+  });
+});
+
+// route GET qui permet de récupérer la semaine en cours
+router.get("/lastsemaine/:token", (req, res) => {
+  User.findOne({ token: req.params.token }).then((user) => {
+    if (user === null) {
+      res.json({ result: false, error: "User not found" });
       return;
     }
-    else {
-      User.updateOne({ token: req.body.token}, 
-      {$set: {semaines: {jour: req.body.jour, nbPersonneSemaine: req.body.nbPersonneSemaine}}})
-    
-      .then((data) => {
-        console.log(data)
-        res.json({ result: true, semaines: data });
-      });}
 
+    User.find({ semaines: req.body.semaines }).then((semaines) => {
+      res.json({ result: true, semaines: semaines.lastIndexOf() });
+    });
+  });
 });
- });
 
+// route Get qui permet de récuperer toutes les semaines enregistrées en favoris
+router.get("/semaines/:token", (req, res) => {
+  User.findOne({ token: req.params.token }).then((user) => {
+    if (user === null) {
+      res.json({ result: false, error: "User not found" });
+      return;
+    }
 
-    // route GET qui permet de récupérer la semaine en cours
-    router.get('/lastsemaine/:token', (req, res) => {
-      User.findOne({ token: req.params.token }).then(user => {
-        if (user === null) {
-          res.json({ result: false, error: 'User not found' });
-          return;
-        }
-
-        User.find({ semaines: req.body.semaines })
-          .then(semaines => {
-            res.json({ result: true, semaines: semaines.lastIndexOf() });
-          });
-      });
+    User.find({ semaines: req.body.semaines }).then((semaines) => {
+      res.json({ result: true, semaines });
     });
+  });
+});
 
-
-    // route Get qui permet de récuperer toutes les semaines enregistrées en favoris
-    router.get('/semaines/:token', (req, res) => {
-      User.findOne({ token: req.params.token }).then(user => {
-        if (user === null) {
-          res.json({ result: false, error: 'User not found' });
-          return;
-        }
-
-        User.find({ semaines: req.body.semaines })
-          .then(semaines => {
-            res.json({ result: true, semaines });
-          });
-      });
-    });
-
-    // route PUT qui permet de modifier la semaine type déjà enrgistrée
-    router.put('/semaines', (req, res) => {
-      User.findOne({ token: req.body.token }).then(user => {
-        if (user === null) {
-          res.json({ result: false, error: 'User not found' });
-          return;
-        }
-        User.findOne(req.body.semaines).then(semaines => {
-          if (!semaines) {
-            res.json({ result: false, error: "Semaine n'existe pas" });
-            return;
-          }
-
-          else {
-            User.updateOne({ semaines: { jour, nbPersonneSemaine } })
-              .then(() => {
-                res.json({ result: true });
-              });
-          }
+// route PUT qui permet de modifier la semaine type déjà enrgistrée
+router.put("/semaines", (req, res) => {
+  User.findOne({ token: req.body.token }).then((user) => {
+    if (user === null) {
+      res.json({ result: false, error: "User not found" });
+      return;
+    }
+    User.findOne(req.body.semaines).then((semaines) => {
+      if (!semaines) {
+        res.json({ result: false, error: "Semaine n'existe pas" });
+        return;
+      } else {
+        User.updateOne({ semaines: { jour, nbPersonneSemaine } }).then(() => {
+          res.json({ result: true });
         });
-      });
+      }
     });
+  });
+});
 
-    module.exports = router;
-    
-
-    
+module.exports = router;
