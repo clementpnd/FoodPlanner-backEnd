ff2e0c9fee4301e688a00f02b6a4f63f89fe11cc
Lempereur <lempereur.julien83@gmail.com>
2023-05-18 15:44:03 +0200

    backend 1.4

--- models/recettes.js
+++ models/recettes.js
@@ -9,12 +9,13 @@ const ingredientsSchema = mongoose.Schema({
 
 const recetteSchema = mongoose.Schema({
   nom: String,
+  type : String,
   description: String,
   temps: String,
   difficulte: String,
   instruction: [String],
   image: String,
-  ingredients: ingredientsSchema,
+  ingredients: [ingredientsSchema],
 });
 
 const Recette = mongoose.model("recettes", recetteSchema);
--- routes/recettes.js
+++ routes/recettes.js
@@ -2,12 +2,41 @@ var express = require("express");
 var router = express.Router();
 const { checkBody } = require("../modules/checkBody");
 const Recette = require("../models/recettes");
+const User = require("../models/users");
 
 //route qui recupère toutes les recettes
 router.get("/", (req, res) => {
+  Recette.find()
+    .populate("ingredients")
+    .then((data) => {
+      res.json({ data: data });
+    });
+});
+
+router.get("/suggestion", (req, res) => {
   Recette.find().then((data) => {
     res.json({ data: data });
   });
 });
 
+router.post("/recettePref", (req, res) => {
+  let pref = [];
+  let responseRecette = [];
+  User.find({ token: req.body.token }).then((data) => {
+      pref = data[0].preference;
+
+  });
+
+  Recette.find().then((data) => {
+    if(pref.length > 0 ){
+    pref.map((prefe) => {
+      responseRecette.push(data.filter((recette) => recette.type === prefe));
+    });
+    res.json({pref : true, responseRecette: responseRecette });
+  }
+  else{
+    res.json({pref : false,responseRecette : data})
+  }
+  });
+});
 module.exports = router;
